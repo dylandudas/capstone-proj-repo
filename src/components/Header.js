26fc26987b24b6760aae5f0080b5d5eb42f2d8dc
Dylan Dudas <dylandudas@gmail.com>
2025-04-04 17:18:16 -0500

    big commit with images and semantic structure

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav"
+import logo from '../assets/logo.jpg'
+
+function Header() {
+    return (
+        <header>
+          <img height={90} src={logo} alt="logo"/>  
+          <Nav/>  
+        </header>
+    )
+}
+
+export default Header;
