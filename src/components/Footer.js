f9b28eac0064a0db5ac61b0fb5a707cb009dbce5
Dylan Dudas <dylandudas@gmail.com>
2025-04-04 19:58:26 -0500

    bit more progress on html

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,39 @@
 function Footer() {
     return (
         <footer>
-
+            <div>
+                <h4>Doormat Navigation</h4>
+                <nav>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Menu</a></li>
+                        <li><a>Reservations</a></li>
+                        <li><a>Order Online</a></li>
+                        <li><a>Login</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <h4>Contact</h4>
+                <nav>
+                    <ul>
+                        <li><a>Address</a></li>
+                        <li><a>Phone Number</a></li>
+                        <li><a>Email</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <h4>Social Media Links</h4>
+                <nav>
+                    <ul>
+                        <li><a>Facebook</a></li>
+                        <li><a>Instagram</a></li>
+                        <li><a>X</a></li>
+                    </ul>
+                </nav>
+            </div>
         </footer>
     )
 }
