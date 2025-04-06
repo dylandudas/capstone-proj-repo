befd908d37876f423cd1dbd9e862fe47989fd15d
Dylan Dudas <dylandudas@gmail.com>
2025-04-06 10:19:30 -0500

    work in progress, baby steps

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,23 @@
-
+import guyholding from '../assets/guyholding.jpg';
 
 function Main() {
     return (
         <main>
+            <section className="hero">
+                <div className="hero-content">
+                    <div className="hero-text">
+                        <h1>Little Lemon</h1>
+                        <p>Chicago</p>
+                        <p style={{color: "white"}}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    </div>
+                    <div className="hero-image">
+                        <img id="heroimage" src={guyholding} alt="Restaurant"/>
+                    </div>
+                </div>
+            </section>
+            <section className="highlights">
 
+            </section>
         </main>
     )
 }
